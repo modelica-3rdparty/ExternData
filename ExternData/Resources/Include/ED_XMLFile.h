e82c87bdeb44751e0af8d1c19a534056ef11c354
tbeu <tc@tbeu.de>
2016-11-09 22:25:51 +0100

    Remove ED_2D

--- ExternData/Resources/Include/ED_XMLFile.h
+++ ExternData/Resources/Include/ED_XMLFile.h
@@ -35,7 +35,6 @@ double ED_getDoubleFromXML(void* _xml, const char* varName);
 const char* ED_getStringFromXML(void* _xml, const char* varName);
 int ED_getIntFromXML(void* _xml, const char* varName);
 void ED_getDoubleArray1DFromXML(void* _xml, const char* varName, double* a, size_t n);
-void ED_getDimDoubleArray2DFromXML(void* _xml, const char* varName, int* dim);
 void ED_getDoubleArray2DFromXML(void* _xml, const char* varName, double* a, size_t m, size_t n);
 
 #endif
