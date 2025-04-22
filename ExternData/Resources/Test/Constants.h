109abb65b0a54b834594f5ba7a3e6550a0e8e47a
Thomas Beutlich <modelica@tbeu.de>
2025-04-22 20:15:31 +0200

    Update testsuite

--- ExternData/Resources/Test/Constants.h
+++ ExternData/Resources/Test/Constants.h
@@ -14,4 +14,7 @@ constexpr const int log_error = 4;
 constexpr const char delimiter_comma[] = ",";
 constexpr const char double_quote[] = "\"";
 
+constexpr const int strict_off = 0;
+constexpr const int strict_on = 1;
+
 }  // namespace
--- ExternData/Resources/Test/Test_ED_INI.cc
+++ ExternData/Resources/Test/Test_ED_INI.cc
@@ -19,6 +19,55 @@ TEST(ED_INI, Create) {
     ED_destroyINI(handle);
 }
 
+TEST(ED_INI, GetDoubleStrict) {
+    auto handle = ED_createINI("../Examples/test.ini", verbose_on, log_debug);
+    ASSERT_NE(nullptr, handle);
+    int exist;
+    auto val = ED_getDoubleFromINI(handle, "gain.k", "", strict_on, &exist);
+    EXPECT_EQ(1.0, val);
+    EXPECT_EQ(1, exist);
+    val = ED_getDoubleFromINI(handle, "gain.k", "set2", strict_on, &exist);
+    EXPECT_EQ(-2.0, val);
+    EXPECT_EQ(1, exist);
+    ED_destroyINI(handle);
+}
+
+TEST(ED_INI, GetIntStrict) {
+    auto handle = ED_createINI("../Examples/test.ini", verbose_on, log_debug);
+    ASSERT_NE(nullptr, handle);
+    int exist;
+    auto val = ED_getIntFromINI(handle, "gain.k", "", strict_on, &exist);
+    EXPECT_EQ(1, val);
+    EXPECT_EQ(1, exist);
+    val = ED_getIntFromINI(handle, "gain.k", "set2", strict_on, &exist);
+    EXPECT_EQ(-2, val);
+    EXPECT_EQ(1, exist);
+    ED_destroyINI(handle);
+}
+
+TEST(ED_INI, GetString) {
+    auto handle = ED_createINI("../Examples/test.ini", verbose_on, log_debug);
+    ASSERT_NE(nullptr, handle);
+    int exist;
+    auto val = ED_getStringFromINI(handle, "gain.k", "", &exist);
+    EXPECT_STREQ("1", val);
+    EXPECT_EQ(1, exist);
+    val = ED_getStringFromINI(handle, "gain.k", "set2", &exist);
+    EXPECT_STREQ("-2", val);
+    EXPECT_EQ(1, exist);
+    ED_destroyINI(handle);
+}
+
+TEST(ED_INI, GetInt) {
+    auto handle = ED_createINI("../Examples/sample.tir", verbose_on, log_debug);
+    ASSERT_NE(nullptr, handle);
+    int exist;
+    auto val = ED_getIntFromINI(handle, "USE_MODE", "MODEL", strict_off, &exist);
+    EXPECT_EQ(14, val);
+    EXPECT_EQ(1, exist);
+    ED_destroyINI(handle);
+}
+
 }  // namespace
 
 int main(int argc, char **argv)
